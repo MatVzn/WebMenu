76ad1a0cb7a94c6c51c814016fde59bc11b09ccf
MatVzn <matteovoleite@gmail.com>
2023-12-22 16:07:44 -0300

    calculation of total price in cart

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var cardapio = {};
 
 var MEU_CARRINHO = [];
 
+var VALOR_CARRINHO = 0;
+var VALOR_ENTREGA = 5;
+
 cardapio.eventos = {
 
   init: () => {
@@ -239,10 +242,17 @@ cardapio.metodos = {
 
         $("#itensCarrinho").append(temp);
 
+        if ((i + 1) == MEU_CARRINHO.length) {
+          // Atualiza o valor total do carrinho
+          cardapio.metodos.carregarValores();
+        }
+
       })
 
     } else {
       $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho está vazio </p>');
+      // Atualiza o valor total do carrinho
+      cardapio.metodos.carregarValores();
     }
 
   },
@@ -289,10 +299,33 @@ cardapio.metodos = {
 
     // Atualiza o botao carrinho com a quantidade atualizada
     cardapio.metodos.atualizarBadgeTotal();
+    
+    // Atualiza o valor total do carrinho
+    cardapio.metodos.carregarValores();
 
   },
 
+  // Carrega os valores do carrinho
+  carregarValores: () => {
 
+    VALOR_CARRINHO = 0;
+
+    $("#lblSubTotal").text(`R$ 0,00`);
+    $("#lblValorEntrega").text(`R$ + 0,00`);
+    $("#lblValorTotal").text(`R$ 0,00`);
+
+    $.each(MEU_CARRINHO, (i, e) => {
+
+      VALOR_CARRINHO += parseFloat(e.price * e.qntd);
+
+      if ((i + 1) == MEU_CARRINHO.length) {
+        $("#lblSubTotal").text(`R$ ${VALOR_CARRINHO.toFixed(2).replace('.', ',')}`);
+        $("#lblValorEntrega").text(`+ R${VALOR_ENTREGA.toFixed(2).replace('.', ',')}`);
+        $("#lblValorTotal").text(`R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}`);
+      }
+    })
+
+  },
 
 
 
