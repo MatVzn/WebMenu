854116a25eaf6285e155e38c247334d551f5f307
MatVzn <matteovoleite@gmail.com>
2023-12-25 00:21:39 -0300

    automatic whatsapp message

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ var MEU_ENDERECO = null;
 var VALOR_CARRINHO = 0;
 var VALOR_ENTREGA = 5;
 
+var CELULAR_EMPRESA = "5521912345678";
+
 cardapio.eventos = {
 
   init: () => {
@@ -234,8 +236,6 @@ cardapio.metodos = {
 
       $.each(MEU_CARRINHO, (i, e) => {
 
-        console.log(/\${img}/g, e.img)
-
         let temp = cardapio.templates.itemCarrinho
           .replace(/\${img}/g, e.img)
           .replace(/\${name}/g, e.name)
@@ -454,8 +454,6 @@ cardapio.metodos = {
 
     $.each(MEU_CARRINHO, (i, e) => {
 
-      console.log(/\${img}/g, e.img)
-
       let temp = cardapio.templates.itemResumo
         .replace(/\${img}/g, e.img)
         .replace(/\${name}/g, e.name)
@@ -469,6 +467,44 @@ cardapio.metodos = {
     $("#resumoEndereco").html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`);
     $("#cidadeEndereco").html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`);
     
+    cardapio.metodos.finalizarPedido();
+
+  },
+
+  // Gera o link para o whatsapp com a mensagem do pedido
+  finalizarPedido: () => {
+
+    if (MEU_CARRINHO.length > 0 && MEU_ENDERECO != null) {
+
+      var texto = 'Olá, gostaria de fazer um pedido:';
+      texto += `\n*Itens do pedido:*\n\n\${itens}`;
+      texto += `\n*Endereço de entrega:*`;
+      texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.bairro}`;
+      texto += `\n${MEU_ENDERECO.cidade}, ${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep} ${MEU_ENDERECO.complemento}`;
+      texto += `\n\n\*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.', ',')}*`;
+
+      var itens = '';
+
+      $.each(MEU_CARRINHO, (i, e) => {
+
+        itens += `*${e.qntd}x* ${e.name} ....... R$ ${e.price.toFixed(2).replace('.', ',')} \n`;
+
+        if ((i + 1) == MEU_CARRINHO.length) {
+
+          texto = texto.replace(/\${itens}/g, itens);
+
+          // converter URL
+          let encode = encodeURI(texto);
+          let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+          $("#btnEtapaResumo").attr('href', URL);
+
+        }
+
+      })
+
+    }
+
   },
 
 
